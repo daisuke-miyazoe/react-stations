d64bd601a552b76db6499db5dfcda643877b6dae
だい <dai@dainonotobukkukonpyuta.local>
2025-08-25 01:13:43 +0900

    day5

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,14 @@ import './App.css'
  */
 export const App = () => {
   const [dogUrl, setDogUrl] = useState('https://images.dog.ceo/breeds/spaniel-brittany/n02101388_6057.jpg')
+  function changeDogUrl(){
+    setDogUrl('https://images.dog.ceo/breeds/hound-english/n02089973_1132.jpg')
+  }
   return (
     <div>
       <header>Dogアプリ</header>
       <div>犬の画像を表示するサイトです</div>
+      <button onClick={changeDogUrl}>更新</button>
       <img src={dogUrl}></img>
     </div>
   )
