8a674424f6dd0579f5de6769da91ada5c4315336
だい <dai@dainonotobukkukonpyuta.local>
2025-08-25 01:06:44 +0900

    day4

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 // DO NOT DELETE
 
+import { useState } from 'react'
 import './App.css'
 
 /**
  * @type {() => JSX.Element}
  */
 export const App = () => {
+  const [dogUrl, setDogUrl] = useState('https://images.dog.ceo/breeds/spaniel-brittany/n02101388_6057.jpg')
   return (
     <div>
       <header>Dogアプリ</header>
       <div>犬の画像を表示するサイトです</div>
-      <img src='https://images.dog.ceo/breeds/spaniel-brittany/n02101388_6057.jpg'></img>
+      <img src={dogUrl}></img>
     </div>
   )
 }
